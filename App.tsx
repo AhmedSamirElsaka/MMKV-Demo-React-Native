d949d5ff85fbe741a8128d773ed30bc2ff5c3008
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-10-01 10:12:49 +0300

    finish test mmkv and make demo

--- App.tsx
+++ App.tsx
@@ -4,115 +4,40 @@
  *
  * @format
  */
+import React, {useState} from 'react';
+import {Button, Text, View} from 'react-native';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {testStore} from './store/mmkv';
+import {useMMKVBoolean, useMMKVNumber, useMMKVString} from 'react-native-mmkv';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+function App(): React.JSX.Element {
+  const [testText, setTestText] = useState(
+    testStore.getString('user.name') || '',
+  );
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  testStore.set('user.name', 'Marc');
+  testStore.set('user.age', 21);
+  testStore.set('is-mmkv-fast-asf', true);
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const username: string = testStore.getString('user.name') || 'ahmed';
+  const age = testStore.getNumber('user.age'); // 21
+  const isMmkvFastAsf = testStore.getBoolean('is-mmkv-fast-asf'); // true
+
+  const [usernameHook, setUsernameHook] = useMMKVString('user.name');
+  const [ageHook, setAgeHook] = useMMKVNumber('user.age');
+  const [isMmkvFastAsfHook, setIsMmkvFastAfHook] =
+    useMMKVBoolean('is-mmkv-fast-asf');
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View>
+      <Text>{testText}</Text>
+      <Button
+        title="Test"
+        onPress={() => {
+          testStore.set('user.name', 'ahmed samir mohamed');
+        }}
+      />
     </View>
   );
 }
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
---
+++ store/mmkv.ts
@@ -0,0 +1,5 @@
+import {MMKV} from 'react-native-mmkv';
+
+export const testStore = new MMKV({
+  id: 'test',
+});
